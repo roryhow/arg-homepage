8dd811e6f9d110fa41caa138ece0210dfe7a5450
Rory How <rhow93@gmail.com>
2018-07-20 13:13:05 +0300

    force html content type on frontend endpoints

--- src/clj/homepage/handler.clj
+++ src/clj/homepage/handler.clj
@@ -1,16 +1,16 @@
 (ns homepage.handler
   (:require [compojure.core :refer [GET POST defroutes rfn]]
             [compojure.route :refer [resources]]
-            [ring.util.response :refer [resource-response]]
+            [ring.util.response :refer [resource-response content-type]]
             [ring.middleware.reload :refer [wrap-reload]]
             [homepage.bot :refer [send-message]]
             [homepage.middleware :refer [wrap-middleware]]))
 
 (defroutes routes
   ;; re-frame application
-  (GET "/" [] (resource-response "index.html" {:root "public"}))
-  (GET "/about" [] (resource-response "index.html" {:root "public"}))
-  (GET "/contact" [] (resource-response "index.html" {:root "public"}))
+  (GET "/" [] (content-type (resource-response "index.html" {:root "public"}) "text/html; charset=utf-8"))
+  (GET "/about" [] (content-type (resource-response "index.html" {:root "public"}) "text/html; charset=utf-8"))
+  (GET "/contact" [] (content-type (resource-response "index.html" {:root "public"}) "text/html; charset=utf-8"))
 
   ;; API
   (POST "/send-message" req (send-message req))
