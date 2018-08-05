e99ffe4bb12a38ec8ccb7224d4f742a0d8123fa6
Rory How <rhow93@gmail.com>
2018-08-05 17:00:32 +0300

    order functions

--- src/cljs/homepage/components/recaptcha.cljs
+++ src/cljs/homepage/components/recaptcha.cljs
@@ -6,17 +6,17 @@
 (defonce has-loaded (r/atom false))
 (defonce is-mounted (atom false))
 
+
+(defn ^:export data_expired_callback [] (dispatch [:homepage.events/set-recaptcha-expired]))
+(defn- render-recaptcha []
+  (data_expired_callback) ;; clear recaptcha results
+  (.render js/grecaptcha "g-recaptcha"
+           (clj->js :sitekey "6LeMcWUUAAAAAOSsfkGq0YQ1aiwEPkrpy_B77jhP")))
 (defn ^:export onload []
   (reset! has-loaded true)
   ;; don't render if component isn't mounted yet
   (when @is-mounted render-recaptcha))
 (defn ^:export data_callback [x] (dispatch [:homepage.events/set-recaptcha-response x]))
-(defn ^:export data_expired_callback [] (dispatch [:homepage.events/set-recaptcha-expired]))
-
-(defn- render-recaptcha []
-  (data_expired_callback) ;; clear recaptcha results
-  (.render js/grecaptcha "g-recaptcha"
-           (clj->js :sitekey "6LeMcWUUAAAAAOSsfkGq0YQ1aiwEPkrpy_B77jhP")))
 
 (defn recaptcha []
   (let [api-loaded? @has-loaded]
