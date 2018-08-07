ef9b2470d6658341ad52bf8207b2eda1212c3d54
Rory How <rhow@FL431.home>
2018-08-07 21:06:37 +0300

    add some console logs to see how things work with the recaptcha

--- src/cljs/homepage/components/recaptcha.cljs
+++ src/cljs/homepage/components/recaptcha.cljs
@@ -3,19 +3,20 @@
             [soda-ash.core :as sa]
             [re-frame.core :refer [dispatch subscribe]]))
 
-(def state (r/atom {:has-loaded false :is-mounted true}))
+(def state (r/atom {:has-loaded false :is-mounted false}))
 
 ;; when the recaptcha response is no longer valid
 (defn ^:export data_expired_callback [] (dispatch [:homepage.events/set-recaptcha-expired]))
 
 ;; render the recaptcha
 (defn- render-recaptcha []
-  (data_expired_callback) ;; clear recaptcha results
+  (dispatch [:homepage.events/set-recaptcha-expired])
   (.render js/grecaptcha "g-recaptcha"
            (clj->js :sitekey "6LeMcWUUAAAAAOSsfkGq0YQ1aiwEPkrpy_B77jhP")))
 
 (defn ^:export onload []
   (swap! state assoc :has-loaded true)
+  (print "loaded!")
   ;; don't render if component isn't mounted yet
   (when (:is-mounted @state) render-recaptcha))
 
@@ -25,7 +26,11 @@
   (let [api-loaded? (:has-loaded @state)]
     (r/create-class
      {:display-name "recaptcha"
-      :component-did-mount #(when api-loaded? (render-recaptcha) (swap! state assoc :is-mounted true))
+      :component-did-mount
+      #(do
+         (render-recaptcha)
+         (print "mounted!")
+         (swap! state assoc :is-mounted true))
       :reagent-render
       (fn [] [sa/FormField {:style {:margin-bottom 0 }}
              [:div.g-recaptcha {:data-sitekey "6LeMcWUUAAAAAOSsfkGq0YQ1aiwEPkrpy_B77jhP"
