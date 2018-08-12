8837fa793696eb98829129707850a2f2f5e6fc04
Rory How <rhow93@gmail.com>
2018-08-12 21:19:14 +0300

    alright I think we're getting somewhere

--- src/cljs/homepage/components/recaptcha.cljs
+++ src/cljs/homepage/components/recaptcha.cljs
@@ -1,9 +1,10 @@
 (ns homepage.components.recaptcha
   (:require [reagent.core :as r]
+            [reagent.ratom :as ra]
             [soda-ash.core :as sa]
             [re-frame.core :refer [dispatch subscribe]]))
 
-(def state (r/atom {:has-loaded false :is-mounted false}))
+(defonce state (ra/atom {:has-loaded false :is-mounted false}))
 
 ;; when the recaptcha response is no longer valid
 (defn ^:export data_expired_callback [] (dispatch [:homepage.events/set-recaptcha-expired]))
@@ -14,27 +15,20 @@
   (.render js/grecaptcha "g-recaptcha"
            (clj->js :sitekey "6LeMcWUUAAAAAOSsfkGq0YQ1aiwEPkrpy_B77jhP")))
 
-(defn ^:export onload []
-  (swap! state assoc :has-loaded true)
-  ;; don't render if component isn't mounted yet
-  ;; (when (:is-mounted @state) render-recaptcha)
-  (println "onload!")
-  (render-recaptcha))
+(defn ^:export onload [] (swap! state assoc :has-loaded true))
 
 (defn ^:export data_callback [x] (dispatch [:homepage.events/set-recaptcha-response x]))
 
 (defn recaptcha []
-  (let [api-loaded? (:has-loaded @state)]
-    (r/create-class
-     {:display-name "recaptcha"
-      :component-did-mount
-      #(do
-         (println "component did mount!")
-         (render-recaptcha)
-         (swap! state assoc :is-mounted true))
-      :reagent-render
-      (fn [] [sa/FormField {:style {:margin-bottom 0 }}
+  (r/create-class
+   {:display-name "recaptcha"
+    :component-did-update #(when (and (:is-mounted @state) (:has-loaded @state))
+                             (render-recaptcha))
+    :component-did-mount  #(swap! state assoc :is-mounted true)
+    :reagent-render
+    (fn [] (let [s @state] ;; subscribe to rerenders
+            [sa/FormField {:style {:margin-bottom 0 }}
              [:div.g-recaptcha {:data-sitekey "6LeMcWUUAAAAAOSsfkGq0YQ1aiwEPkrpy_B77jhP"
                                 :data-callback "onDataCallback"
                                 :data-expired-callback "onExpiredCallback"
-                                :id "g-recaptcha"}]])})))
+                                :id "g-recaptcha"}]]))}))
